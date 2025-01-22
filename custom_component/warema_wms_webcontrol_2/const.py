ddb97ebd73d29f868eacf40217cb47de94cf87d0
Hubert Becker <hu@hjpb.de>
2025-01-22 01:26:20 +0100

    update

--- custom_component/warema_wms_webcontrol_2/const.py
+++ custom_component/warema_wms_webcontrol_2/const.py
@@ -1,6 +1,6 @@
 """Constants"""
-CONF_WEBCONTROL_SERVER_ADDR = 'webcontrol_server_addr'
-CONF_UPDATE_INTERVAL = 'update_interval'
+CONF_WEBCONTROL_SERVER_ADDR = "webcontrol_server_addr"
+CONF_UPDATE_INTERVAL = "update_interval"
 
 BASE_URL = "http://www.wienerlinien.at/ogd_realtime/monitor?rbl={}"
 
